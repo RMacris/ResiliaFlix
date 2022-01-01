4de9cca3fd14bcd5facf92c4703544d5dc74758d
Rafael Soares <rafa.macris@gmail.com>
2022-01-01 14:34:33 -0300

    get movie id from url implemented for movie page

--- src/js/movie-page.js
+++ src/js/movie-page.js
@@ -1,9 +1,22 @@
 
+
+
+
 window.onload = async function() { 
     let view = new MoviePageView()
     let controller = new MovieRequestController();
-
+    let movieId = GetMovieIdFromUrl()
+    let response = await controller.requestById(movieId)
     let response = await controller.requestById_Test()
 
     view.PopualtePage(response)
+}
+
+
+function GetMovieIdFromUrl(){ 
+    let url = new URL(window.location.href);
+
+    // gets the id from the page url 
+    let id = url.searchParams.append('i', 42);
+    return id
 }
