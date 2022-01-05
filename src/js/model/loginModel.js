4b5cb864a91df1af84055d256875a8af93df42b0
Mauriciiow <mauricioliveira639@gmail.com>
2022-01-05 16:13:42 -0300

    corrigi erro de redirecionamento

--- src/js/model/loginModel.js
+++ src/js/model/loginModel.js
@@ -2,7 +2,7 @@ class LoginModel{
     validaDadosLogin(email, senha){
         try {
             if (email.indexOf('@') > -1 && senha.length >= 8) {
-                 return window.location.href = '/src/html/main-page/main-page.html'
+                 return window.location.href = '../../html/main-page/main-page.html'
                 
             } else {
                  throw new Error('Dados incorretos')
