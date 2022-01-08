21c17f418e0f2c21bb6da66c8a5bb1221925c905
Rafael Soares <rafa.macris@gmail.com>
2022-01-07 23:10:07 -0300

    js refatoring

--- src/js/search.js
+++ src/js/search.js
@@ -10,20 +10,13 @@ function WaitInputFinishToRequest(e) {
     if(e.target.value.lenght == 0 ) return;
     
     clearTimeout(time)
+
+    //wait 0.5s after stop pressing key before trigger the function
     time = setTimeout(async () => {
         // read input 
         const targetValue = e.target.value.toString()
-        console.log(targetValue)
+        HandleSearch('../search-result/search-result-page.html','i','s',targetValue)
 
-        if(!window.location.href.includes('search-result-page')){
-            RemoveUrlParams('i')
-            RedirectAndAddUrlQuery('../search-result/search-result-page.html', 's', targetValue)
-
-            console.log('está na search result page')
-        }else{
-            console.log('está na search result page')
-            HandleSearch(targetValue)
-        }
 
     }, 500);
 }
@@ -33,48 +26,29 @@ function SearchOnPressEnter(e) {
     if(e.target.value.lenght == 0 ) return;
     if (e.keyCode === 13) {
         const targetValue = e.target.value.toString()
-        console.log(targetValue)
-        console.log('Enter is pressed!');
-        if(!window.location.href.includes('search-result-page')){
-            RemoveUrlParams('i')
-            RedirectAndAddUrlQuery('../search-result/search-result-page.html', 's', targetValue)
-        }else{
-            RemoveUrlParams('i')
-            RedirectAndAddUrlQuery('../search-result/search-result-page.html', 's', targetValue)
-        }
+        HandleSearch('../search-result/search-result-page.html','i','s',targetValue)
+
     }
 }
 
 function SearchOnBtnClick(){
     if($('#BtnSearch').val().length > 0) return;
     const targetValue = $('BtnSearch').val()
-    console.log(targetValue)
-    console.log('Enter is pressed!');
+    HandleSearch('../search-result/search-result-page.html','i','s',targetValue)
+   
+}
+
+
+function HandleSearch(redirectPath, removeKey, newKey, targetValue ) {
     if(!window.location.href.includes('search-result-page'))
     {
-        RemoveUrlParams('i')
-        RedirectAndAddUrlQuery('../search-result/search-result-page.html', 's', targetValue)
+        RemoveUrlParams(removeKey)
+        RedirectAndAddUrlQuery(redirectPath, newKey, targetValue)
     }
     else
     {
-        RemoveUrlParams('i')
-        RedirectAndAddUrlQuery('../search-result/search-result-page.html', 's', targetValue)
+        RemoveUrlParams(removeKey)
+        RedirectAndAddUrlQuery(redirectPath, newKey, targetValue)
     }
 }
-
-async function HandleSearch(search){ 
-    
-    // if in search-result-page
-    console.log(search)
-    //request
-    let movieRequestController = new MovieRequestController();
-    const result = await movieRequestController.resquestByTitle(search)
-
-    console.log(result)
-
-    //card creation 
-    let factory = new CardFactory()
-    let card = factory.CardFactory(result)
-
-    //TODO: for each card append in the dom on the search result page
-}
+ 
